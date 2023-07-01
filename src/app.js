2dc2cc21f6a769d7d0168e00d9d19603bdb7dbd8
dthcs <dthcsc@gmail.com>
2023-07-01 09:11:56 +0900

    layout

--- src/app.js
+++ src/app.js
@@ -9,44 +9,44 @@ const dotenv = require('dotenv');
 dotenv.config();
 const pageRouter = require('./routes/page');
 
-const app = express()
+const app = express();
 app.set('port', process.env.PORT || 8001);
 app.set('view engine', 'html');
 nunjucks.configure('views', {
-    express: app,
-    watch: true,
+  express: app,
+  watch: true,
 });
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser(process.envy.COOKIE_SECRET));
+app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(session({
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.COOKIE_SECRET,
-    cookie: {
-        httpOnly: true,
-        secure: false,
-    },
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
 }));
 
 app.use('/', pageRouter);
 
 app.use((req, res, next) => {
-    const error = new Error(`${req.method} ${req.url} do not have router`);
-    error.status = 404;
-    next(error);
+  const error =  new Error(`${req.method} ${req.url} no router.`);
+  error.status = 404;
+  next(error);
 });
 
 app.use((err, req, res, next) => {
-    res.locals.message = err.message;
-    req.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
-    res.status(err.status || 500);
-    res.render('error');
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 app.listen(app.get('port'), () => {
-    console.log(app.get('port'), 'port waiting');
+  console.log(app.get('port'), 'port waiting');
 });
